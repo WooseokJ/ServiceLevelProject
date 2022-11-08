82207ae99eac2a9db3571c4540acfea8008bbcce
Wooseok <wooseokbird@gmail.com>
2022-11-09 00:02:09 +0900

    Add: 인증번호 뷰 추가

---
+++ ServiceLevelProject/Presentation/Auth/View/AuthViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AuthViewController.swift
+//  ServiceLevelProject
+//
+//  Created by useok on 2022/11/08.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    let authView = AuthView()
+    
+    override func loadView() {
+        super.view = authView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
