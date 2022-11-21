a32dea17609b1883a4994ced7a1166e1115190a7
Wooseok <wooseokbird@gmail.com>
2022-11-21 17:14:20 +0900

    Add: 검색 화면 UI

--- ServiceLevelProject/Presentation/Search/View/SearchView.swift
+++ ServiceLevelProject/Presentation/Search/View/SearchView.swift
@@ -5,4 +5,144 @@
 //  Created by useok on 2022/11/21.
 //
 
-import Foundation
+import SnapKit
+import UIKit
+
+class SearchView: BaseView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        setConstrains()
+    }
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "띄어쓰기로 복수 입력이 가능해요"
+        return searchBar
+    }()
+    lazy var searchButton: UIButton = {
+        let bt = UIButton()
+        bt.setTitle("새싹찾기", for: .normal)
+        bt.backgroundColor = BrandColor.green
+        bt.setTitleColor(BlackWhite.white, for: .normal)
+        bt.layer.cornerRadius = 10
+        bt.layer.borderWidth = 1
+        bt.layer.borderColor = UIColor.clear.cgColor
+        bt.clipsToBounds = true
+        return bt
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let spacing : CGFloat = 1
+        let layoutwidth = UIScreen.main.bounds.width
+        let layoutheight = UIScreen.main.bounds.height
+        layout.itemSize = CGSize(width: layoutwidth / 2.5  , height: layoutheight / 29 )
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.register(InfoManageMentCollectionViewCell.self, forCellWithReuseIdentifier: InfoManageMentCollectionViewCell.reuseIdentifier)
+        cv.layer.cornerRadius = 10
+        cv.clipsToBounds = true
+        cv.backgroundColor = .clear
+        cv.register(HeaderCollectionView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionView.reuseIdentifier)
+        return cv
+    }()
+    
+    
+    override func configure() {
+        [searchBar,collectionView,searchButton].forEach {
+            self.addSubview($0)
+        }
+    }
+    
+    override func setConstrains() {
+        searchButton.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.trailing.equalTo(-16)
+            make.bottom.equalTo(-50)
+            make.height.equalTo(UIScreen.main.bounds.height * 0.06)
+        }
+        collectionView.snp.makeConstraints { make in
+            make.bottom.equalTo(searchButton.snp.top).offset(-10)
+            make.leading.equalTo(16)
+            make.trailing.equalTo(-16)
+            make.top.equalTo(self.safeAreaLayoutGuide)
+            
+        }
+    }
+    
+}
+
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0 : return InfoManageMent.title.list.count
+        case 1: return InfoManageMent.sesacStudy.list.count
+        default: return 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: InfoManageMentCollectionViewCell.reuseIdentifier, for: indexPath) as? InfoManageMentCollectionViewCell else{return UICollectionViewCell()}
+        switch indexPath.section {
+            
+        case 0:
+            cell.itemButton.setTitle(InfoManageMent.title.list[indexPath.row], for: .normal)
+        case 1:
+            cell.itemButton.setTitle(InfoManageMent.sesacStudy.list[indexPath.row], for: .normal)
+        default:
+            break
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionView.reuseIdentifier, for: indexPath) as? HeaderCollectionView else {return HeaderCollectionView()}
+        switch indexPath.section {
+            case 0 :header.label.text = "지금 주변에는"
+            case 1: header.label.text = "내가 하고 싶은"
+            default: break
+        }
+        header.backgroundColor = .clear
+        header.configure()
+        header.layoutSubviews()
+        return header
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 100, height: 30)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: InfoManageMentCollectionViewCell.reuseIdentifier, for: indexPath) as? InfoManageMentCollectionViewCell else { return .zero}
+        cell.itemButton.sizeToFit()
+        let layoutwidth = UIScreen.main.bounds.width
+        let layoutheight = UIScreen.main.bounds.height
+        switch indexPath.section {
+        case 0: return CGSize(width: InfoManageMent.title.list[indexPath.row].size(withAttributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14)]).width + 40, height: 32)
+        case 1:
+            return CGSize(width: InfoManageMent.sesacStudy.list[indexPath.row].size(withAttributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14)]).width + 40, height: 32)
+        default:
+            print("오류",#function)
+            return CGSize()
+
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 10, left: 0, bottom: 30, right: 0)
+        }
+    
+    
+    
+    
+    
+}
--- ServiceLevelProject/Presentation/Search/View/SearchViewController.swift
+++ ServiceLevelProject/Presentation/Search/View/SearchViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: BaseViewController {
+
+    let searchView = SearchView()
+    override func loadView() {
+        super.view = searchView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchView.collectionView.delegate = self
+        searchView.collectionView.dataSource = self
+        
+        self.hideKeyboardWhenTappedAround()
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        self.navigationItem.titleView = searchView.searchBar
+        
     }
     
+//    @objc func keyboardWillShow(_ sender: Notification) {
+//        searchView.searchButton.frame.origin.y = -0.01
+//        }
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        self.view.endEditing(true)
+//    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+//        self.view.endEditing(true)
+//    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
     }
-    */
-
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
